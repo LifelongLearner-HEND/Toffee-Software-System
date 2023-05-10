fbc1a995fb6cee5206070e2232d4333aef304044
Hend Ahmed <92755637+LifelongLearner-HEND@users.noreply.github.com>
2023-05-10 16:15:03 +0300

    Add files via upload

---
+++ Implementation/Main.java
@@ -0,0 +1,9 @@
+import CustomerService.Database;
+
+public class Main {
+    public static void main(String[] args) {
+        Application app = new Application() {};
+        Database database = new Database();
+        app.run();
+    }
+}
