246b18acf606271025ee2b5692c40b603022ab74
Hend Ahmed <92755637+LifelongLearner-HEND@users.noreply.github.com>
2023-05-20 16:55:57 +0300

    Add files via upload

--- Implementation/Cart Item Manager/Cart.java
+++ Implementation/Cart Item Manager/Cart.java
@@ -3,9 +3,17 @@ package CartItemManager;
 import java.util.HashMap;
 import static CustomerService.Database.catalog;
 
+/**
+ * This class is used to manage the cart items,It will add, remove, search for items in the cart, It will also check if the quantity is valid, It will also check if the item is available in the catalog or not, It will also check if the cart is empty or not, It will also check if the item is available in the cart or not
+ */
 public class Cart {
     private HashMap<Item, Integer> cartItems = new HashMap<Item, Integer>();
 
+    /**
+     * This method is used to add item to the cart, It will check if the item is available in the catalog or not, If yes, it will check if the quantity is valid or not, If yes, it will check if the item is available in the catalog or not, If yes, it will add the item to the cart
+     * @param name
+     * @param quantity
+     */
     public void addItem(String name, int quantity) {
         Item item = searchOnCatalog(name);
         if (item != null) {
@@ -21,22 +29,36 @@ public class Cart {
             System.out.println("Item not found!!");
         }
     }
+
+    /**
+     * This method is used to check if the cart is empty or not
+     * @return
+     */
     public boolean isEmpty() {
         return (cartItems.size() == 0);
     }
+
+    /**
+     * This method is used to remove item from the cart, It will check if the cart is empty or not, If yes, it will check if the item is available in the cart or not, If yes, it will remove the item from the cart, If no, it will print out the message "Item not found!!"
+     * @param name
+     */
     public void removeItem(String name) {
         if (isEmpty()) {
             System.out.println("Cart is empty!!");
             return;
         }
-        if (searchOnCart(name) != null) {
+        else if (searchOnCart(name) != null) {
             cartItems.remove(searchOnCart(name));
-//            System.out.println("Item removed from cart successfully ☺");
         }
         else {
             System.out.println("Item not found!!");
         }
     }
+    /**
+     * This method is used to search for item in the catalog
+     * @param itemName
+     * @return
+     */
     public Item searchOnCatalog(String itemName) {
         for (Item item : catalog) {
             if (item.getName().equals(itemName)) {
@@ -46,6 +68,11 @@ public class Cart {
         return null;
     }
 
+    /**
+     * This method is used to search for item in the cart
+     * @param name
+     * @return
+     */
     public Item searchOnCart(String name) {
         for (Item item : cartItems.keySet()) {
             if (item.getName().equals(name)) {
@@ -55,14 +82,29 @@ public class Cart {
         return null;
     }
 
+    /**
+     * This method is used to check if the quantity is valid or not( between 1 and 50)
+     * @param quantity
+     * @return
+     */
     private boolean isValidQuantity(int quantity) {
         return (quantity > 0 && quantity <= 50);
     }
 
+    /**
+     * This method is used to check if the item is available in the catalog or not
+     * @param item
+     * @param quantity
+     * @return
+     */
     private boolean isAvailable(Item item, int quantity) {
         return (item.getNumberOfUnits() >= quantity);
     }
 
+    /**
+     * This method is used to get the cart items
+     * @return
+     */
     public HashMap<Item, Integer> getCartItems() {
         return cartItems;
     }
--- Implementation/Cart Item Manager/Item.java
+++ Implementation/Cart Item Manager/Item.java
@@ -1,5 +1,8 @@
 package CartItemManager;
 
+/**
+ * this class is the item class, it will store the information of the item
+ */
 public class Item {
     private String name;
     private int numberOfUnits;
@@ -7,6 +10,14 @@ public class Item {
     private String brand;
     private double discountPercentage;
 
+    /**
+     * this is the constructor of the item class
+     * @param name
+     * @param numberOfUnits
+     * @param price
+     * @param brand
+     * @param discountPercentage
+     */
     public Item(String name, int numberOfUnits, double price, String brand, double discountPercentage) {
         this.name = name;
         this.numberOfUnits = numberOfUnits;
@@ -14,18 +25,42 @@ public class Item {
         this.brand = brand;
         this.discountPercentage = discountPercentage;
     }
+
+    /**
+     * this method is used to get the name of the item
+     * @return
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * this method is used to get the number of units of the item
+     * @return
+     */
     public int getNumberOfUnits() {
         return numberOfUnits;
     }
+    /**
+    * this method is used to get the price of the item
+    * @return
+    */
     public double getPrice() {
         return price;
     }
+
+    /**
+     * this method is used to get the brand of the item
+     * @return
+     */
     public String getBrand() {
         return brand;
     }
+
+    /**
+     * this method is used to get the discount percentage of the item
+     * @return
+     */
     public double getDiscountPercentage() {
         return discountPercentage;
     }
